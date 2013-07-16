659c858ada587f7402a26485b7f14d0f797ab504
jonathonleyh <jonathonleyh@gmail.com>
2013-07-15 21:50:26 -0700

    Temporarily remove password recover

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable
 
 end
+
+#:recoverable,
