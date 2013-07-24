39b4a509892ad286e3804f5937f40960f2e95e62
jonathonleyh <jonathonleyh@gmail.com>
2013-07-23 21:40:57 -0700

    Initial Commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130716043832) do
+ActiveRecord::Schema.define(version: 20130717034815) do
+
+  create_table "goals", force: true do |t|
+    t.string   "title"
+    t.string   "summary"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
