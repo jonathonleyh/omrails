26a2860e848ca34f3998bbc76524a6927c690e84
jonathonleyh <jonathonleyh@gmail.com>
2013-07-29 21:08:15 -0700

    Probably screwin up migration files

--- db/migrate/20130730035803_add_user_id_to_pins.rb
+++ db/migrate/20130730035803_add_user_id_to_pins.rb
@@ -1,6 +1,5 @@
 class AddUserIdToPins < ActiveRecord::Migration
   def change
-    add_column :pins, :user_id, :integer
-    add_index :pins, :user_id
+   
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130730031223) do
+ActiveRecord::Schema.define(version: 20130730035803) do
 
   create_table "goals", force: true do |t|
     t.string   "title"
